cd8f5470d16195887489553e00cf23fe2ba6159c
contactboudiss <65862709+contactboudiss@users.noreply.github.com>
2021-08-21 23:21:54 +0200

    love

---
+++ boudiss.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication13
+{
+    public class boudiss
+    {
+        private int b;
+
+        public boudiss( int bb)
+        {
+            b = bb;
+        }
+
+        public int B { get => b; set => b = value; }
+    }
+}
